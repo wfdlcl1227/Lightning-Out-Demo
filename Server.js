57631ac341bee56cc3cc555eee3426437318e0e3
wfdlcl1227@126.com <wfdlcl1227@126.com>
2021-05-27 23:13:42 +0800

    123

--- Server.js
+++ Server.js
@@ -78,7 +78,7 @@ app.get('/oauthcallback.html' ,  function(req,res,next) {
 
 app.get('/Main*' ,   function(req,res,next) {
 	//res.sendfile('views/test.html');
-    getSFToken(req, res);
+    //getSFToken(req, res);
 } );
  
 app.get('/WW_verify_e6WJnTuiAA4sEwBY.txt' ,   function(req,res,next) {
